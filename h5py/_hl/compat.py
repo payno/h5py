545277a809dcbaea3aa20fd95027f15f9515a498
juliagarriga <julia.garriga@esrf.fr>
2019-09-19 11:42:34 +0200

    [python2 clean] Deleted compatibility function for python2 with fsencode and fsdecode

--- h5py/_hl/compat.py
+++ h5py/_hl/compat.py
@@ -43,61 +43,8 @@ except ImportError:
 
 # This is from python 3.5 stdlib (hence lacks PEP 519 changes)
 # This was introduced into python 3.2, so python < 3.2 does not have this
-# Effectively, this is only required for python 2.6 and 2.7, and can be removed
-# once support for them is dropped
-def _fscodec():
-    encoding = sys.getfilesystemencoding()
-    if encoding == 'mbcs':
-        errors = 'strict'
-    else:
-        try:
-            from codecs import lookup_error
-            lookup_error('surrogateescape')
-        except LookupError:
-            errors = 'strict'
-        else:
-            errors = 'surrogateescape'
-
-    def fsencode(filename):
-        """
-        Encode filename to the filesystem encoding with 'surrogateescape' error
-        handler, return bytes unchanged. On Windows, use 'strict' error handler if
-        the file system encoding is 'mbcs' (which is the default encoding).
-        """
-        if isinstance(filename, six.binary_type):
-            return filename
-        elif isinstance(filename, six.text_type):
-            return filename.encode(encoding, errors)
-        else:
-            raise TypeError("expect bytes or str, not %s" % type(filename).__name__)
-
-    def fsdecode(filename):
-        """
-        Decode filename from the filesystem encoding with 'surrogateescape' error
-        handler, return str unchanged. On Windows, use 'strict' error handler if
-        the file system encoding is 'mbcs' (which is the default encoding).
-        """
-        if isinstance(filename, six.text_type):
-            return filename
-        elif isinstance(filename, six.binary_type):
-            return filename.decode(encoding, errors)
-        else:
-            raise TypeError("expect bytes or str, not %s" % type(filename).__name__)
-
-    return fsencode, fsdecode
-
-_fsencode, _fsdecode = _fscodec()
-del _fscodec
-
-try:
-    from os import fsencode
-except ImportError:
-    fsencode = _fsencode
-
-try:
-    from os import fsdecode
-except ImportError:
-    fsdecode = _fsdecode
+from os import fsencode
+from os import fsdecode
 
 
 def filename_encode(filename):
